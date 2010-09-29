d14cf3b3b4bb2d112a032e925d00a9334714f588
Jens Wille <jens.wille@uni-koeln.de>
2010-09-29 18:46:15 +0200

    _plugins/prometheus.rb: Moved pagination fix to localization plugin. Cleaned up a bit.

--- _plugins/prometheus.rb
+++ _plugins/prometheus.rb
@@ -4,31 +4,17 @@ require 'jekyll/localization'
 
 module Jekyll
 
-  class Page
-
-    alias_method :_prometheus_original_initialize, :initialize
-
-    def initialize(site, base, dir, name)
-      _prometheus_original_initialize(site, base, dir, name)
-
-      pandora_url  = @site.config['pandora_url']
-      pandora_url += "/#{@lang}" if pandora_url && @lang
-      @pandora_url = data['pandora_url'] = pandora_url
-    end
-
-  end
-
   class Post
 
     def title
       to_hash['title']
     end
 
-    alias_method :_localization_original_url, :url
+    alias_method :_prometheus_original_url, :url
 
-    # Overwrites the original method to include the language extension.
+    # Overwrites the original method to drop the language extension.
     def url
-      _localization_original_url.sub(/\.\w{2}\z/, '')
+      _prometheus_original_url.sub(/#{Localization::LANG_EXT_RE}\z/, '')
     end
 
     alias_method :_prometheus_original_write, :write
@@ -39,11 +25,9 @@ module Jekyll
       FileUtils.mkdir_p(File.join(dest, dir))
 
       # The url needs to be unescaped in order to preserve the correct filename
-      path = File.join(dest, CGI.unescape(self.url + '.html' + @lang_ext))
+      path = File.join(dest, CGI.unescape("#{url}.html#{@lang_ext}"))
 
-      File.open(path, 'w') do |f|
-        f.write(self.output)
-      end
+      File.open(path, 'w') { |f| f.write(output) }
     end
 
   end
@@ -61,36 +45,6 @@ module Jekyll
     end
 
     alias_method :p, :pandora
-
-  end
-
-  class Pagination < Generator
-
-    alias_method :_prometheus_original_paginate, :paginate
-
-    # Overwrites the original method to prevent double posts in pagination
-    def paginate(site, page)
-      all_posts = site.site_payload['site']['posts']
-      all_posts = uniq_by_url(all_posts)
-      pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
-      (1..pages).each do |num_page|
-        pager = Pager.new(site.config, num_page, all_posts, pages)
-        if num_page > 1
-          newpage = Page.new(site, site.source, page.dir, page.name)
-          newpage.pager = pager
-          newpage.dir = File.join(page.dir, "page#{num_page}")
-          site.pages << newpage
-        else
-          page.pager = pager
-        end
-      end
-    end
-
-    def uniq_by_url(posts)
-      uniq_posts = {}
-      posts.each { |p| uniq_posts[p.url] = p }
-      uniq_posts.values.sort_by { |p| p.date }.reverse
-    end
 
   end
 
