6a2fe88d4ca406803f4fd5216caddaa01d4347a2
Arne Eilermann <eilermann@lavabit.com>
2010-10-05 18:59:08 +0200

    Typos corrected.

--- _plugins/prometheus.rb
+++ _plugins/prometheus.rb
@@ -93,7 +93,7 @@ module Jekyll
     end
 
     def render_navigation_item(item)
-      state = active_itme?(item) ? ' active' : ''
+      state = active_item?(item) ? ' active' : ''
       if item[:url]
         name = "<a href=\"#{relativize(item[:url], path)}\">" +
           "#{item["title_#{lang}".to_sym]}</a>"
@@ -101,7 +101,7 @@ module Jekyll
         name = item["title_#{lang}".to_sym]
       end
 
-      if item[:content].nil? || !opend?(item)
+      if item[:content].nil? || !open?(item)
         "<li class=\"navigation_item#{state}\">#{name}</li>\n"
       else
         "<li class=\"navigation_item#{state}\">#{name}\n" +
@@ -121,14 +121,14 @@ module Jekyll
       @path ||= File.join(@dir, basename)
     end
 
-    def active_itme?(navigation_item)
+    def active_item?(navigation_item)
       navigation_item[:url] == path[1..-1]
     end
 
-    def opend?(navigation_item)
-      return true if active_itme?(navigation_item)
+    def open?(navigation_item)
+      return true if active_item?(navigation_item)
       if navigation_item[:content]
-        navigation_item[:content].each { |i| return true if opend?(i) }
+        navigation_item[:content].each { |i| return true if open?(i) }
       end
 
       return false
