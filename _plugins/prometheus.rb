47656d6ae18e8e68a3bf328d091b17ff29b9660d
Jens Wille <jens.wille@uni-koeln.de>
2010-10-05 16:21:14 +0200

    Added page_title helper.

--- _plugins/prometheus.rb
+++ _plugins/prometheus.rb
@@ -35,16 +35,31 @@ module Jekyll
   module Filters
 
     def relativize(str)
-      '../' * (@page.url.count('/') - 1) + str.sub(/\A\//, '')
+      '../' * (page.url.count('/') - 1) + str.sub(/\A\//, '')
     end
 
     alias_method :r, :relativize
 
     def pandora(*path)
-      [@site.pandora_url, @page.lang, *path].compact.join('/')
+      [site.pandora_url, page.lang, *path].compact.join('/')
     end
 
     alias_method :p, :pandora
+
+    def page_title(head = false)
+      parts, title = page.url.sub(/\A\//, '').split('/'), page.title
+
+      case parts.first
+        when /\A\d+\z/
+          pre = "#{t('prometheus Blog', 'prometheus-Blog')} - " if head
+          "#{pre}#{title}"
+        when 'series'
+          num = parts.values_at(-2, -3).join(' / ')
+          "#{t('Image series', 'Bildserie')} #{num}: #{title}"
+        else
+          title
+      end
+    end
 
   end
 
