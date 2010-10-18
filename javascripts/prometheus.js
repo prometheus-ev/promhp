1adf4ee30ee93b22b7dca1bd259590bd64789443
Jens Wille <jens.wille@uni-koeln.de>
2010-10-18 17:43:01 +0200

    Remove nopandora text from HTML source. Search engines will pick it up. That's no good...

--- javascripts/prometheus.js
+++ javascripts/prometheus.js
@@ -1,7 +1,17 @@
 var Prometheus = {};
 
+Prometheus.lang = 'en';
+
 Prometheus.nopandora = function() {
   var flag = true;
+  var text = Prometheus.lang === 'de' ?
+    "<strong>Das Bildarchiv steht Ihnen zur Zeit leider nicht zur Verfügung!</strong>\n" +
+    "Wir bemühen uns, den Betrieb schnellstmöglich wiederherzustellen. Bitte\n" +
+    "entschuldigen Sie die Unannehmlichkeiten.\n<br /><br />Ihr prometheus-Team"
+  :
+    "<strong>The image archive is currently not available!</strong>\n" +
+    "We're sorry about this inconvenience and try to recover the\n" +
+    "service as soon as possible.\n<br /><br />Your prometheus Team"
 
   $$('.nopandora.hidden').each(function(i) {
     if (flag) {
@@ -9,6 +19,6 @@ Prometheus.nopandora = function() {
       $$('.pandoraonly').invoke('hide');
     }
 
-    i.removeClassName('hidden');
+    i.update(text).removeClassName('hidden');
   });
 };
