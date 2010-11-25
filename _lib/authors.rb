5889673c3c0765c31d8fafbecb685983a1e86823
Arne Eilermann <eilermann@lavabit.com>
2010-11-25 16:57:27 +0100

    Some minor enhancements.

--- _lib/authors.rb
+++ _lib/authors.rb
@@ -3,20 +3,20 @@ module Jekyll
   class AuthorGenerator < Generator
 
     def generate(site)
-      site.config['authors'] = YAML.load_file(File.join(site.source, '_authors.yml'))
-      author_pages(site)
+      @authors = site.config['authors'] = YAML.load_file(File.join(site.source, '_authors.yml'))
+      generate_author_pages(site)
     end
 
     def get_posts(site, author, lang)
-      posts = []
-      site.posts.each { |p| posts << p if p.data['author'] == author && p.lang == lang }
-      posts.sort
+      site.posts.map { |p|
+        p if p.data['author'] == author && p.lang == lang
+      }.compact.sort.reverse
     end
 
-    def author_pages(site)
-      return unless site.config['authors']
+    def generate_author_pages(site)
+      return unless @authors
       Localization::LANGUAGES.each { |lang|
-        site.config['authors'].each { |a|
+        @authors.each { |a|
           a[1]['posts'] = get_posts(site, a[0], lang)
           site.pages << AuthorPage.new(site, site.source, '/blog/author/', "#{a[0].downcase}.#{lang}.html",
             {
