23670687205b0a8496f30138416a653870838852
Jens Wille <jens.wille@uni-koeln.de>
2010-11-29 16:47:55 +0100

    Changed nopandora to require manual intervention so it's more reliable.
    Automatic detection of pandora's availability doesn't seem to work for all users. We get lots of false negatives. So instead, we make the default look like the noscript version (mostly) and insert the warning message manually when we *know* that pandora is not running: Remove 'NOPANDORA' string from index.html.{en,de}.

--- javascripts/prometheus.js
+++ javascripts/prometheus.js
@@ -1,24 +1,3 @@
 var Prometheus = {};
 
 Prometheus.lang = 'en';
-
-Prometheus.nopandora = function() {
-  var flag = true;
-  var text = Prometheus.lang === 'de' ?
-    "<strong>Das Bildarchiv steht Ihnen zur Zeit leider nicht zur Verfügung!</strong>\n" +
-    "Wir bemühen uns, den Betrieb schnellstmöglich wiederherzustellen. Bitte\n" +
-    "entschuldigen Sie die Unannehmlichkeiten.\n<br /><br />Ihr prometheus-Team"
-  :
-    "<strong>The image archive is currently not available!</strong>\n" +
-    "We're sorry about this inconvenience and try to recover the\n" +
-    "service as soon as possible.\n<br /><br />Your prometheus Team"
-
-  $$('.nopandora.hidden').each(function(i) {
-    if (flag) {
-      flag = false;
-      $$('.pandoraonly').invoke('hide');
-    }
-
-    i.update(text).removeClassName('hidden');
-  });
-};
