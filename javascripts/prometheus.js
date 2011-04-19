414d301b8e72d1fa83a64f25d87cf0d58cf6395b
Jens Wille <jens.wille@uni-koeln.de>
2011-04-19 13:05:51 +0200

    _includes/statusbar.html: Added locale switcher. (Requires jekyll-localization >= 0.1.2)

--- javascripts/prometheus.js
+++ javascripts/prometheus.js
@@ -1,3 +1,20 @@
-var Prometheus = {};
+var Prometheus = {
 
-Prometheus.lang = 'en';
+  lang: 'en',
+
+  switch_locale_switcher: function(restore) {
+    var s = $('locale_switcher');
+    if (s) {
+      if (restore) {
+        var c = this.locale_switcher_content;
+        if (c) {
+          s.innerHTML = c;
+        }
+      }
+      else {
+        this.locale_switcher_content = s.innerHTML;
+      }
+    }
+  }
+
+};
