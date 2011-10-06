cc77089e1eecc97bd0cf79852ae6a794bb987c70
Arne Eilermann <eilermann@lavabit.com>
2011-10-06 16:16:15 +0200

    Added a switch to view the website in standard mode on mobile devices.

--- javascripts/prometheus.js
+++ javascripts/prometheus.js
@@ -15,6 +15,29 @@ var Prometheus = {
         this.locale_switcher_content = s.innerHTML;
       }
     }
+  },
+
+  switch_to_standard_mode: function() {
+    var expire_at = new Date();
+    expire_at.setDate(expire_at.getDate() + 1);
+    var value = "standard-mode; expires=" + expire_at.toUTCString();
+    document.cookie = value;
+
+    this.remove_mobile_stylesheet();
+  },
+
+  check_view_mode: function() {
+    var cookie = document.cookie.split(";");
+
+    for (i = 0; i < cookie.length;i++) {
+      if (cookie[i] == 'standard-mode') {
+        this.remove_mobile_stylesheet();
+      }
+    }
+  },
+
+  remove_mobile_stylesheet: function() {
+    $('mobil-stylesheet').remove();
   }
 
 };
