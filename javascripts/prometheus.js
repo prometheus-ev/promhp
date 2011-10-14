8697915c1efdbfaba3e2e1dee9717563cb1c5000
Jens Wille <jens.wille@uni-koeln.de>
2011-10-14 13:24:51 +0200

    Revert "javascripts/prometheus.js: Removed switch_locale_switcher; unused."
    D'oh! It's used from pandora...
    
    This reverts commit d9b9a8b8637cdd1a7619b8b5f331379683c16b28.

--- javascripts/prometheus.js
+++ javascripts/prometheus.js
@@ -2,6 +2,21 @@ var Prometheus = {
 
   lang: 'en',
 
+  switch_locale_switcher: function(restore) {
+    var s = $('locale_switcher');
+    if (s) {
+      if (restore) {
+        var c = this.locale_switcher_content;
+        if (c) {
+          s.innerHTML = c;
+        }
+      }
+      else {
+        this.locale_switcher_content = s.innerHTML;
+      }
+    }
+  },
+
   switch_to_standard_mode: function() {
     var expire_at = new Date();
     expire_at.setDate(expire_at.getDate() + 1);
