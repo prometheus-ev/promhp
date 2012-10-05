3c20bdef18ad75e6c4149dc7707af8bfebee9ae7
Jens Wille <jens.wille@uni-koeln.de>
2012-10-05 12:46:47 +0200

    _lib/tagger.rb: Updated for jekyll-tagging v0.4.0.

--- _lib/tagger.rb
+++ _lib/tagger.rb
@@ -4,18 +4,14 @@ module Jekyll
 
     alias_method :_prometheus_original_generate_tag_pages, :generate_tag_pages
 
-    def generate_tag_pages(site)
-      tags, original_tags = [], site.tags.dup
+    def generate_tag_pages
+      site.tags, original_tags = tags = [], site.tags.dup
 
-      Localization::LANGUAGES.each { |lang|
-        original_tags.each { |tag, posts|
-          posts = posts.select { |p| p.lang == lang }
-          tags << ["#{tag}.#{lang}", posts]
-        }
-      }
+      Localization::LANGUAGES.each { |lang| original_tags.each { |tag, posts|
+        tags << ["#{tag}.#{lang}", posts.select { |post| post.lang == lang }]
+      } }
 
-      site.tags = tags
-      _prometheus_original_generate_tag_pages(site)
+      _prometheus_original_generate_tag_pages
     ensure
       site.tags = original_tags if original_tags
     end
